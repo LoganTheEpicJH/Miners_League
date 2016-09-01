1010c845e3912f0eb2b0b0a4de1ca591be7c4aef
LoganTheEpicJH <epicloganjh@yahoo.com>
2016-09-01 11:03:37 -0500

    Build 3
    build 3

--- src/com/minersleague/main/commands/CMD_Announcement.java
+++ src/com/minersleague/main/commands/CMD_Announcement.java
@@ -1,19 +1,34 @@
 package com.minersleague.main.commands;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Sound;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+
+import com.minersleague.main.util.Utilities;
 
 public class CMD_Announcement extends MinersLeagueCommand {
 	public CMD_Announcement() {
 		super("announce");
 	}
 
+	@SuppressWarnings("deprecation")
 	@Override
 	public boolean onCommand(CommandSender sender, String[] args) {
 		Player p = (Player) sender;
 		
-		if (p.hasPermission("")) {
+		if (p.hasPermission("minersleague.rank.admin")) {
+			String message = args.toString();
 			
+			for (Player pp : Bukkit.getOnlinePlayers()) {
+				pp.sendMessage("");
+				pp.sendMessage(Utilities.color("&b&lANNOUNCEMENT"));
+				pp.sendMessage(Utilities.color(message));
+				pp.sendMessage(Utilities.color("&b&lANNOUNCEMENT"));
+				pp.sendMessage("");
+				pp.sendTitle(Utilities.color("&b&lANNOUNCEMENT"), Utilities.color(message));
+				pp.playSound(pp.getLocation(), Sound.ENTITY_ENDERDRAGON_GROWL, 1, 0);
+			}
 		}
 		
 		
