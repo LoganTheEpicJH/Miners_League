15e5e882026acb1c428bd8156e8e7fb3b3a164b3
FelixVogel <entelp2803@gmail.com>
2016-09-01 12:49:32 +0200

    Update

--- src/com/minersleague/main/commands/CMD_Test.java
+++ src/com/minersleague/main/commands/CMD_Test.java
@@ -3,6 +3,7 @@ package com.minersleague.main.commands;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import com.minersleague.main.config.Variables;
 import com.minersleague.main.util.Utilities;
 
 public class CMD_Test extends MinerLeagueCommand {
@@ -19,9 +20,14 @@ public class CMD_Test extends MinerLeagueCommand {
 				target.sendMessage(Utilities.color("&6"+sender.getName()+" >> You&f Hi"));
 				sender.sendMessage(Utilities.color("&6You >> "+target.getName()+" &fHi"));
 				return true;
+			} else {
+				sender.sendMessage(Utilities.color("&cThe Player &4"+args[0]+" &cis not Online!"));
+				return true;
 			}
+		} else {
+			sender.sendMessage(Variables.MISSING_ARGUMENTS_1);
+			return true;
 		}
-		return false;
 	}
 
 }
---
+++ src/com/minersleague/main/config/Variables.java
@@ -0,0 +1,11 @@
+package com.minersleague.main.config;
+
+import com.minersleague.main.util.Utilities;
+
+public class Variables {
+
+	public static String NO_PERMISSION_1 = Utilities.color("&cYou dont have the required Permission");
+	public static String WRONG_ARGUMENTS_1 = Utilities.color("&cWrong Arguments");
+	public static String MISSING_ARGUMENTS_1 = Utilities.color("&cCommand is missing Arguments");
+	
+}
