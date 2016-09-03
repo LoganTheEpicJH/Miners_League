7272bea078c5d790b057f40ea5a83d027b82193d
FelixVogel <entelp2803@gmail.com>
2016-09-03 06:20:31 +0200

    Build 10l

--- src/com/minersleague/main/towerdefense/GameStarter.java
+++ src/com/minersleague/main/towerdefense/GameStarter.java
@@ -136,7 +136,7 @@ public class GameStarter implements Runnable {
 //				}
 //			}
 			try {
-				Thread.sleep(10);
+				Thread.sleep(5);
 			} catch(InterruptedException e) {
 				e.printStackTrace();
 			}
--- src/com/minersleague/main/towerdefense/Rounds.java
+++ src/com/minersleague/main/towerdefense/Rounds.java
@@ -2,8 +2,11 @@ package com.minersleague.main.towerdefense;
 
 import java.util.ArrayList;
 
+import org.bukkit.Bukkit;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Zombie;
+
+import com.minersleague.main.Main;
 
 public class Rounds {
 
@@ -23,12 +26,42 @@ public class Rounds {
 	public void nextRound(Game game) {
 		if(roundAt<=maxRound) {
 			//int zPerRound = 1;//10*roundAt;
-			zombies.add((Zombie)game.getStart().getLocation().getWorld().spawnEntity(game.getStart().getLocation(), EntityType.ZOMBIE));
-			zombies.add((Zombie)game.getStart().getLocation().getWorld().spawnEntity(game.getStart().getLocation(), EntityType.ZOMBIE));
-			zombies.add((Zombie)game.getStart().getLocation().getWorld().spawnEntity(game.getStart().getLocation(), EntityType.ZOMBIE));
-			zombies.add((Zombie)game.getStart().getLocation().getWorld().spawnEntity(game.getStart().getLocation(), EntityType.ZOMBIE));
-			zombies.add((Zombie)game.getStart().getLocation().getWorld().spawnEntity(game.getStart().getLocation(), EntityType.ZOMBIE));
-			zombies.add((Zombie)game.getStart().getLocation().getWorld().spawnEntity(game.getStart().getLocation(), EntityType.ZOMBIE));
+			Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(Main.plugin, new Runnable() {
+				@Override
+				public void run() {
+					zombies.add((Zombie)game.getStart().getLocation().getWorld().spawnEntity(game.getStart().getLocation(), EntityType.ZOMBIE));
+				}
+			}, 10);
+			Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(Main.plugin, new Runnable() {
+				@Override
+				public void run() {
+					zombies.add((Zombie)game.getStart().getLocation().getWorld().spawnEntity(game.getStart().getLocation(), EntityType.ZOMBIE));
+				}
+			}, 30);
+			Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(Main.plugin, new Runnable() {
+				@Override
+				public void run() {
+					zombies.add((Zombie)game.getStart().getLocation().getWorld().spawnEntity(game.getStart().getLocation(), EntityType.ZOMBIE));
+				}
+			}, 50);
+			Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(Main.plugin, new Runnable() {
+				@Override
+				public void run() {
+					zombies.add((Zombie)game.getStart().getLocation().getWorld().spawnEntity(game.getStart().getLocation(), EntityType.ZOMBIE));
+				}
+			}, 70);
+			Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(Main.plugin, new Runnable() {
+				@Override
+				public void run() {
+					zombies.add((Zombie)game.getStart().getLocation().getWorld().spawnEntity(game.getStart().getLocation(), EntityType.ZOMBIE));
+				}
+			}, 90);
+			Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(Main.plugin, new Runnable() {
+				@Override
+				public void run() {
+					zombies.add((Zombie)game.getStart().getLocation().getWorld().spawnEntity(game.getStart().getLocation(), EntityType.ZOMBIE));
+				}
+			}, 110);
 //			for(int i = 1; i<zPerRound; i++) {
 //				Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(Main.plugin, new Runnable() {
 //					@Override
